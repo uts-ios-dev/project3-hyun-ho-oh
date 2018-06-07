dbeff33fac9d13e8bbc23409ca9f350f8ebbfc45
ohhobs <ohhobs@gmail.com>
2018-06-07 11:32:06 +1000

    Add files via upload

--- Memorable/Memorable/View Controllers/QuizViewController.swift
+++ Memorable/Memorable/View Controllers/QuizViewController.swift
@@ -140,6 +140,32 @@ class QuizViewController: UIViewController, UIPickerViewDataSource, UIPickerView
 
 }
 
+extension MutableCollection where Indices.Iterator.Element == Index {
+    /// Shuffles the contents of this collection.
+    mutating func shuffle() {
+        let c = count
+        guard c > 1 else { return }
+        
+        for (firstUnshuffled , unshuffledCount) in zip(indices, stride(from: c, to: 1, by: -1)) {
+            // Change `Int` in the next line to `IndexDistance` in < Swift 3.2
+            let d: Int = numericCast(arc4random_uniform(numericCast(unshuffledCount)))
+            guard d != 0 else { continue }
+            let i = index(firstUnshuffled, offsetBy: d)
+            self.swapAt(firstUnshuffled, i)
+        }
+    }
+}
+
+extension Sequence {
+    /// Returns an array with the contents of this sequence, shuffled.
+    func shuffled() -> [Iterator.Element] {
+        var result = Array(self)
+        result.shuffle()
+        return result
+    }
+}
+
+/*
 extension MutableCollection {
     /// Shuffles the contents of this collection.
     mutating func shuffle() {
@@ -163,3 +189,4 @@ extension Sequence {
         return result
     }
 }
+ */
